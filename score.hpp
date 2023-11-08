dd03017d3325f2f9e3c0bc565b6f4106d59c420e
sarahfaisal <faisalsara124@gmail.com>
2023-11-08 13:16:20 +0500

    ui header file

--- score.hpp
+++ score.hpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Score {
+class Score: public UI {
     private:
         int currentScore;
 
--- time.hpp
+++ time.hpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Time {
+class Time: public UI {
     private:
         int timeRemaining;
     public:
