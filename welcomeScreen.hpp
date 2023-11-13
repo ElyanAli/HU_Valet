c0b178adf08c4a5581e5ed1712e2f31000a6d9d0
sarahfaisal <faisalsara124@gmail.com>
2023-11-13 22:23:14 +0500

    Level and related hpp files

---
+++ welcomeScreen.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "levelButton.hpp"
+#include "changeColourButton.hpp"
+using namespace std;
+
+class welcomeScreen{
+    private:
+    levelButton levelButtons;
+    changeColourButton button_1;
+    changeColourButton button_2;
+    public:
+    void displayOptions();
+
+
+};
