35ddf042582de89036477db4aac991e0adc42486
codeme5 <ma08056@st.habib.edu.pk>
2023-11-24 10:35:56 +0500

    levels updated

---
+++ level.cpp
@@ -0,0 +1,35 @@
+#include "level.hpp"
+
+Level::Level(SDL_Renderer* r, int level, parkingSpot* pSpot, string imagePath){
+    path = imagePath;
+    parking = pSpot;
+    levelNumber = level;
+    renderer = r; 
+};
+void Level::loadObstacles(){
+    for(int i = 0; i < obstacles.size(); i++){
+        obstacles[i].loadObstacle();
+    }
+}
+// void Level::loadCoins(){
+//     for(int i = 0; i < coins.size(); i++){
+//         coins[i].loadCoin();
+//     }
+// }
+void Level::insertObstacle(SDL_Renderer* r, string pth, SDL_Rect rct, SDL_Rect mrct){
+    obstacles.push_back(Obstacle(r, pth, rct, mrct));
+}
+void Level::loadLevel(){
+    // load the image of the level
+    SDL_Surface *imageSurface = SDL_LoadBMP(path.c_str());
+    image = SDL_CreateTextureFromSurface(renderer, imageSurface);
+    SDL_FreeSurface(imageSurface);
+    SDL_RenderCopy(renderer, image, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    // load the obstacles
+    loadObstacles();
+    // load the coins
+    
+    // loadCoins()
+    
+}
---
+++ level.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include "objects.hpp"
+#include "obstacles.hpp"
+#include "coins.hpp"
+#include "parkingSpot.hpp"
+using namespace std;
+
+class Level{
+    private:
+        int levelNumber;
+        vector<Obstacle> obstacles;
+        vector<Coins> coins;
+        parkingSpot* parking;
+        SDL_Renderer* renderer;
+        string path;
+        SDL_Texture* image;
+    public:
+        Level(SDL_Renderer*, int, parkingSpot*, string);
+        void loadObstacles();
+        // void loadCoins();
+        void insertObstacle(SDL_Renderer* r, string pth, SDL_Rect rct, SDL_Rect mrct);
+        void loadLevel();
+
+};
