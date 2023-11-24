b10155748cc8e42eb40a84cabd52d692a6d0f4f8
codeme5 <ma08056@st.habib.edu.pk>
2023-11-24 11:38:11 +0500

    updated accelerations

--- car.cpp
+++ car.cpp
@@ -9,93 +9,28 @@ Car::Car (SDL_Renderer* Rndr, string path, SDL_Rect sRect, SDL_Rect mRect) : Obj
     theta = 0;
     delta = 0;
     center ={21,37};
-    // steering = 0;
 }
 
 void Car::draw() {
-    // Objects::draw();
-    // SDL_RenderCopy(renderer, image, &srcRect, &moverRect);
-    // cout<<moverRect<<endl;
-    
+    moverRect.x += static_cast<int>(velocity * sin(angle*(M_PI/180)));
+    moverRect.y -= static_cast<int>(velocity * cos(angle*(M_PI/180)));
     SDL_RenderCopyEx(renderer, image, &srcRect, &moverRect, angle, &center, SDL_FLIP_NONE);
     cout<<angle<<endl;
 }
 
-// void Car::update(double v, double phi) {
-//     double width = 42.0;
-//     double height = 74.0;
-
-// // Calculate the length of the car
-//     double L = sqrt(pow(width, 2) + pow(height, 2));
-
-// // Calculate the distance from the rear axle to the center of gravity
-//     double lr = 0.4 * L;
-
-
-//     // Update the state based on the bicycle model equations
-//     double R = L / tan(delta);
-//     double beta = atan(lr * tan(delta) / L);
-
-//     // Corrected computation of x_dot and y_dot based on the orientation (theta) of the car
-//     double x_dot = v * cos(theta + beta);
-//     double y_dot = v * sin(theta + beta);
-//     double theta_dot = v * tan(delta) / L;
-//     double delta_dot = phi;
-
-//     moverRect.x += static_cast<int>(x_dot * DELTA_T); // Cast to int for rendering
-//     moverRect.y += static_cast<int>(y_dot * DELTA_T); // Cast to int for rendering
-//     theta += theta_dot * DELTA_T;
-//     delta += delta_dot * DELTA_T;
-
-//     // Wrap angle to the range [0, 2*pi)
-//     theta = fmod(theta, 2 * M_PI);
-//     if (theta < 0)
-//         theta += 2 * M_PI;
-
-//     angle = theta * 180 / M_PI;
-
-//     // Debug prints
-//     cout << "X: "<< moverRect.x<<", y: "<<moverRect.y<<", Angle: "<<angle<< ", x_dot: " << x_dot << ", y_dot: " << y_dot << ", theta_dot: " << theta_dot << ", delta_dot: " << delta_dot << endl;
-// }
-
-// void Car::accelerate(int direction) {
-//     acceleration = direction;
-// }
-
-// void Car::turn(int s) {
-//     steering = s;
-// }
-
-// float Car::getAngle() {
-//     return angle;
-// }
-
-// SDL_Point Car::getAxis() {
-//     return axis;
-// }
-
-// void Car::update(float delta) {
-//     speed += acceleration + delta + max_speed;
-//     const float steering_speed = .5f;
-//     if (speed > max_speed) speed = max_speed;
-//     angle += steering * steering_speed;
-//     moverRect.x += cos(angle) + speed + delta;
-//     moverRect.y += sin(angle) + speed + delta;
-
-//     acceleration = 0;
-//     steering = 0;
-// }
 
 void Car::accelerate() {
-    
-    moverRect.x += static_cast<int>(SPEED * sin(angle*(M_PI/180)));
-    moverRect.y -= static_cast<int>(SPEED * cos(angle*(M_PI/180)));
+    if (velocity <= 20){
+        velocity += acceleration;
+    }
+    moverRect.x += static_cast<int>(velocity * sin(angle*(M_PI/180)));
+    moverRect.y -= static_cast<int>(velocity * cos(angle*(M_PI/180)));
 }
-void Car::deaccelerate() {
-    // moverRect.x -= static_cast<int>(SPEED * cos(angle));
-    // moverRect.y -= static_cast<int>(SPEED * sin(angle));
-    moverRect.x -= static_cast<int>(SPEED * sin(angle*(M_PI/180)));
-    moverRect.y += static_cast<int>(SPEED * cos(angle*(M_PI/180)));
+void Car::decelerate() {
+    if (velocity >= -20){
+        velocity -= acceleration;
+    }
+    
 }
 void Car::turnLeft() {
     angle -= ANGLE_CHANGE;
@@ -104,9 +39,17 @@ void Car::turnRight() {
     angle += ANGLE_CHANGE;
 }
 void Car::releaseAccelerate() {
-    // moverRect.y = moverRect.y - 1;
+    while(velocity>0){
+        velocity -= acceleration;
+        draw();
+    }
 }
-void Car::releaseBreak() {}
+void Car::releaseDeceleration() {
+    while(velocity<0){
+        velocity += acceleration;
+        draw();
+    }
+}
 void Car::straighten() {
     // angle -= 5;
 }
--- car.hpp
+++ car.hpp
@@ -3,15 +3,15 @@
 #include <vector>
 #include <math.h>
 #include "objects.hpp"
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 using namespace std;
 
 class Car : public Objects { //inherit from object
     private:
         // float acceleration;
         
-        const double SPEED = 5.0;
+        const double acceleration = 2;
         const double ANGLE_CHANGE = 5;
         int fuel;
         int collisionState;
@@ -19,44 +19,18 @@ class Car : public Objects { //inherit from object
         SDL_Point center;
         double theta, delta;
         const double DELTA_T = 1.0;
+        int velocity = 0;
 
-
-        // SDL_Point axis = {13/2, 10};
-        // int steering;
-        // float speed;
-        // float angle;
-        // float acceleration = 5;
-        // float max_speed = 10;
-
-    
-
-        // SDL_Rect moverRect;
-        // const float dT;
-        // double frontDistance;
-        // double rearDistance;
-        // double slip;
-        // double acceleration_input;
-        // double steerAngle_input;
     public:
-        // void moveFront();
-        // void moveBack();
-        // void moveRight(bool direction);
-        // void moveLeft(bool direction);
-        // void upState();
+        
         Car (SDL_Renderer* Rndr, string path, SDL_Rect sRect, SDL_Rect mRect);
         void draw();
-        // void accelerate(int direction);
-        // void turn(int s);
-        // SDL_Point getAxis();
-        // float getAngle();
-
-        // void update(double v, double phi);
         void accelerate();
-        void deaccelerate();
+        void decelerate();
         void turnLeft();
         void turnRight();
         void releaseAccelerate();
-        void releaseBreak();
+        void releaseDeceleration();
         void straighten();
 
 };
