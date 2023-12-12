ba8e194393e2f9d10dfe71935ce03b38f8d9f0a0
sarahfaisal <faisalsara124@gmail.com>
2023-12-12 16:37:55 +0500

    digits and score updated
    score class implemented with improved widgets

--- digits.hpp
+++ digits.hpp
@@ -2,10 +2,10 @@
 #include <iostream>
 #include <vector>
 #include "objects.hpp"
-// #include <SDL2/SDL.h>
-// #include <SDL2/SDL_image.h>
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+// #include <SDL.h>
+// #include <SDL_image.h>
 using namespace std;
 
 class Digits : public Objects { //inherit from object
@@ -13,31 +13,22 @@ class Digits : public Objects { //inherit from object
         // int imgState = 0;
         string path = "images/digits.png";
         vector<SDL_Rect> digitSRect = {
-                                        {392, 0, 30, 35}, //0
-                                        {5, 0, 16, 35},   //1
-                                        {38, 0, 28, 35},  //2
-                                        {80, 0, 28, 35},  //3
-                                        {124, 0, 32, 35}, //4
-                                        {170, 0, 28, 35}, //5
-                                        {214, 0, 28, 35}, //6
-                                        {257, 0, 28, 35}, //7
-                                        {301, 0, 30, 35}, //8
-                                        {347, 0, 28, 35}  //9
+                                        {918, 0, 69, 82}, //0
+                                        {13, 1, 35, 81},   //1
+                                        {90, 0, 64, 82},  //2
+                                        {188, 1, 65, 81},  //3
+                                        {291, 1, 74, 81}, //4
+                                        {399, 1, 64, 81}, //5
+                                        {501, 0, 66, 82}, //6
+                                        {603, 1, 65, 81}, //7
+                                        {706, 0, 68, 82}, //8
+                                        {812, 0, 66, 82}  //9
         };
         int count = 0;
-        // int timerCount = 0;
-        // bool collected = false;
-        // void updateMoverRect(int);
+        SDL_Rect getMover(int, int);
     public:
         // friend class CollisionManager;
-        Digits(SDL_Renderer* Rndr, SDL_Rect mRect);
-        void drawDigits();
-        void incDigit(int, int, int);
-        // void accelerate();
-        // void decelerate();
-        // void turnLeft();
-        // void turnRight();
-        // void releaseAccelerate();
-        // void releaseDeceleration();
+        Digits(SDL_Renderer* Rndr);
+        void drawDigit(int digit, int posX, int posY);
 
 };
